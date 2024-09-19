fc6298cae055ba31a0879ea0a5b3ac556ebdaf0a
Tim Hrdy <tim.hrdy.1@gmail.com>
2024-09-20 00:33:55 +0200

    final-back-tofix

--- retrometroid/backend/models/Pricing.js
+++ retrometroid/backend/models/Pricing.js
@@ -1,20 +1,38 @@
 import { Schema, model } from "mongoose";
 
+const VariationSchema = new Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+const OptionSchema = new Schema({
+  variations: [VariationSchema], // Utilisation du sous-schéma pour les variations
+  price: { type: Number, default: 0 }, // Prix de base si aucune variation n'est choisie
+});
+
 const PricingConfigSchema = new Schema({
-  refersto: { type: String, default: "GBA", required: true }, // Par exemple : GBA, GameboyColor, etc.
+  refersto: { type: String, default: "GBA", required: true },
   options: {
+    case: OptionSchema,
+    screen: OptionSchema,
+    buttons: OptionSchema,
+    pads: OptionSchema,
+    gurt: OptionSchema,
+    specialCase: OptionSchema,
+    stickers: OptionSchema,
     consoleToBuy: { price: { type: Number, default: 40 } },
     batteryInstall: { price: { type: Number, default: 40 } },
-    backCase: { price: { type: Number, default: 11.9 } },
+    backCase: OptionSchema, // Utilisation du schéma Option
     ledInstall: { price: { type: Number, default: 50 } },
+    ledTriggerInstall: OptionSchema, // Utilisation du schéma Option
     ampAudio: { price: { type: Number, default: 25 } },
-    // Ajouter d'autres options ici
+    dpadInstall: { price: { type: Number, default: 25 } },
+    stickers: { perso: { price: { type: Number, default: 12.9 } } },
   },
   accessories: {
     RetroMetroidBag: { price: { type: Number, default: 12.9 } },
     TemperedGlass: { price: { type: Number, default: 4.9 } },
     SiliconCase: { price: { type: Number, default: 6.9 } },
-    // Ajouter d'autres accessoires ici
   },
   createdAt: { type: Date, default: Date.now },
 });
--- retrometroid/backend/routers/gbaRouter.js
+++ retrometroid/backend/routers/gbaRouter.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import pkg from "@woocommerce/woocommerce-rest-api";
 import dotenv from "dotenv";
-import PricingConfig from "../models/Pricing.js"; // Assure-toi d'importer ton modèle
 import Pricing from "../models/Pricing.js";
 
 dotenv.config(); // Charger les variables d'environnement
@@ -18,6 +17,74 @@ const router = Router();
 
 const sendProductToWooCommerce = async (product) => {
   try {
+    // Crée un tableau d'attributs sans inclure les attributs vides ou indéfinis
+    const attributes = [
+      { name: "Case", options: [product.customOptions.case], visible: true },
+      {
+        name: "Back Case",
+        options: [product.customOptions.backCase],
+        visible: true,
+      },
+      {
+        name: "Screen",
+        options: [product.customOptions.screen],
+        visible: true,
+      },
+      {
+        name: "Buttons",
+        options: [product.customOptions.buttons],
+        visible: true,
+      },
+      { name: "Pads", options: [product.customOptions.pads], visible: true },
+      { name: "Gurt", options: [product.customOptions.gurt], visible: true },
+      {
+        name: "Stickers",
+        options: [product.customOptions.stickers],
+        visible: true,
+      },
+      {
+        name: "Battery Install",
+        options: [product.customOptions.batteryInstall.toString()],
+        visible: true,
+      },
+      {
+        name: "Special Case",
+        options: [product.customOptions.specialCase.toString()],
+        visible: true,
+      },
+      {
+        name: "LED Install",
+        options: [product.customOptions.ledInstall.toString()],
+        visible: true,
+      },
+      {
+        name: "LED Trigger Install",
+        options: [product.customOptions.ledTriggerInstall.toString()],
+        visible: true,
+      },
+      {
+        name: "DPAD Install",
+        options: [product.customOptions.dpadInstall.toString()],
+        visible: true,
+      },
+      {
+        name: "Amp Audio",
+        options: [product.customOptions.ampAudio.toString()],
+        visible: true,
+      },
+      {
+        name: "Accessories",
+        options: [product.customOptions.accessories.toString()],
+        visible: true,
+      },
+    ];
+
+    // Filtrer pour ne garder que les attributs non vides
+    const filteredAttributes = attributes.filter(
+      (attr) =>
+        attr.options && attr.options[0] !== "" && attr.options[0] !== "false" // Filtrer les options vides ou "false"
+    );
+
     // Créer le produit simple sur WooCommerce
     const wooProduct = await api.post("products", {
       name: product.name,
@@ -26,56 +93,10 @@ const sendProductToWooCommerce = async (product) => {
       description: "Produit personnalisé créé via API.",
       images: [
         {
-          id: 19,
-          //src: "https://api-retrometroid.devprod.fr/wp-content/uploads/2024/09/product-store-07.jpg", // URL de l'image
+          id: 19, // Par exemple, spécifier l'ID de l'image
         },
       ],
-      // Inclure les options sélectionnées comme attributs visibles sur la page produit
-      attributes: [
-        { name: "Case", options: [product.customOptions.case], visible: true },
-        {
-          name: "Back Case",
-          options: [product.customOptions.backCase],
-          visible: true,
-        },
-        {
-          name: "Screen",
-          options: [product.customOptions.screen],
-          visible: true,
-        },
-        {
-          name: "Buttons",
-          options: [product.customOptions.buttons],
-          visible: true,
-        },
-        { name: "Pads", options: [product.customOptions.pads], visible: true },
-        { name: "Gurt", options: [product.customOptions.gurt], visible: true },
-        {
-          name: "Stickers",
-          options: [product.customOptions.stickers],
-          visible: true,
-        },
-        {
-          name: "Battery Install",
-          options: [product.customOptions.batteryInstall.toString()],
-          visible: true,
-        },
-        {
-          name: "LED Install",
-          options: [product.customOptions.ledInstall.toString()],
-          visible: true,
-        },
-        {
-          name: "Amp Audio",
-          options: [product.customOptions.ampAudio.toString()],
-          visible: true,
-        },
-        {
-          name: "Accessories",
-          options: [product.customOptions.accessories.toString()],
-          visible: true,
-        },
-      ],
+      attributes: filteredAttributes, // Utilise les attributs filtrés
     });
 
     const productId = wooProduct.data.id; // Récupère l'ID du produit créé
@@ -88,59 +109,251 @@ const sendProductToWooCommerce = async (product) => {
 
     return addToCartUrl; // Retourner l'URL pour redirection
   } catch (error) {
-    console.error(
-      "Erreur lors de la création du produit sur WooCommerce:",
-      error.response ? error.response.data : error.message
-    );
-    throw error;
+    console.error("Erreur lors de l'envoi du produit à WooCommerce:", error);
   }
 };
 
 // Utiliser la configuration des prix pour calculer le prix final
 router.post("/addProduct", async (req, res) => {
-  try {
-    const { name, customOptions } = req.body;
+  const { name, customOptions } = req.body;
 
-    const pricingConfigGBA = new Pricing();
+  try {
+    // Vérifier si une configuration pour "GBA" existe déjà
+    let pricingConfigGBA = await Pricing.findOne({ refersto: "GBA" });
+
+    if (!pricingConfigGBA) {
+      // Si la configuration n'existe pas, la créer et la sauvegarder
+      pricingConfigGBA = new Pricing({
+        refersto: "GBA",
+        options: {
+          case: {
+            variations: [
+              { name: "black", price: 0 },
+              { name: "white", price: 0 },
+              { name: "rose", price: 0 },
+              { name: "turquoise", price: 0 },
+              { name: "yellow", price: 0 },
+              { name: "ghost", price: 0 },
+              { name: "clear-violet", price: 0 },
+              { name: "clear-orange", price: 0 },
+              { name: "clear-black", price: 0 },
+              { name: "clear-blue", price: 0 },
+              { name: "clear-glass", price: 0 },
+              { name: "clear-green", price: 0 },
+              { name: "clear-red", price: 0 },
+            ],
+          },
+          screen: {
+            variations: [
+              { name: "black", price: 0 },
+              { name: "white", price: 0 },
+            ],
+          },
+          buttons: {
+            variations: [
+              { name: "black", price: 0 },
+              { name: "white", price: 0 },
+              { name: "sness", price: 0 },
+              { name: "blue", price: 0 },
+              { name: "orange", price: 0 },
+              { name: "yellow", price: 0 },
+              { name: "violet", price: 0 },
+              { name: "rose", price: 0 },
+              { name: "red", price: 0 },
+              { name: "lemon", price: 0 },
+              { name: "clear-black", price: 0 },
+              { name: "clear-blue", price: 0 },
+              { name: "clear-glass", price: 0 },
+              { name: "clear-green", price: 0 },
+              { name: "clear-pink", price: 0 },
+            ],
+          },
+          pads: {
+            variations: [
+              { name: "black", price: 0 },
+              { name: "light-blue", price: 0 },
+              { name: "blue", price: 0 },
+              { name: "green", price: 0 },
+              { name: "orange", price: 0 },
+              { name: "red", price: 0 },
+              { name: "rose", price: 0 },
+              { name: "white", price: 0 },
+              { name: "yellow", price: 0 },
+            ],
+          },
+          gurt: {
+            variations: [
+              { name: "black", price: 0 },
+              { name: "rose", price: 0 },
+              { name: "white", price: 0 },
+              { name: "yellow", price: 0 },
+              { name: "violet", price: 0 },
+              { name: "orange", price: 0 },
+              { name: "red", price: 0 },
+              { name: "blue", price: 0 },
+              { name: "green", price: 0 },
+            ],
+          },
+          specialCase: {
+            variations: [
+              { name: "SNESS", price: 16.5 },
+              { name: "FAMICOM", price: 16.5 },
+            ],
+          },
+          stickers: {
+            variations: [
+              { name: "black", price: 0 },
+              { name: "white", price: 0 },
+              { name: "perso", price: 20 },
+            ],
+          },
+          backCase: {
+            variations: [
+              { name: "black", price: 11.9 },
+              { name: "white", price: 11.9 },
+              { name: "rose", price: 11.9 },
+              { name: "turquoise", price: 11.9 },
+              { name: "yellow", price: 11.9 },
+              { name: "ghost", price: 11.9 },
+              { name: "clear-violet", price: 11.9 },
+              { name: "clear-orange", price: 11.9 },
+              { name: "clear-black", price: 11.9 },
+              { name: "clear-blue", price: 11.9 },
+              { name: "clear-glass", price: 11.9 },
+              { name: "clear-green", price: 11.9 },
+              { name: "clear-red", price: 11.9 },
+            ],
+          },
+          ledTriggerInstall: {
+            default: { name: "red", price: 18 }, // Valeur par défaut
+            variations: [
+              { name: "red", price: 18 },
+              { name: "blue", price: 20 },
+              { name: "green", price: 22 },
+            ],
+          },
+        },
+        accessories: {
+          RetroMetroidBag: { price: 12.9 },
+          TemperedGlass: { price: 4.9 },
+          SiliconCase: { price: 6.9 },
+        },
+      });
+
+      await pricingConfigGBA.save();
+      console.log("Config de prix GBA enregistrée avec succès");
+    } else {
+      console.log("Configuration de prix GBA récupérée :");
+    }
 
     // Calculer le prix final à partir des options
     let finalPrice = 149; // Prix de base du produit
+    const validAttributes = {};
+    const invalidAttributes = [];
 
-    // Calculer le prix des options
     for (const [option, value] of Object.entries(customOptions)) {
       if (pricingConfigGBA.options[option]) {
+        const optionConfig = pricingConfigGBA.options[option];
+
+        // Si aucune valeur n'est fournie, utiliser la valeur par défaut
+        if (value === undefined || value === "") {
+          if (optionConfig.default) {
+            value = optionConfig.default.name;
+            console.log(
+              `Aucune valeur fournie pour ${option}. Utilisation de la valeur par défaut: ${value}`
+            );
+          } else {
+            invalidAttributes.push({
+              option,
+              value: "Aucune valeur et pas de valeur par défaut",
+            });
+            continue;
+          }
+        }
+
         if (typeof value === "boolean" && value === true) {
-          finalPrice += pricingConfigGBA.options[option].price; // Ajoute le prix si true
-        } else if (
-          typeof value === "string" &&
-          pricingConfigGBA.options[option][value]
-        ) {
-          finalPrice += pricingConfigGBA.options[option][value].price; // Ajoute le prix basé sur la valeur de l'option
+          finalPrice += optionConfig.price || 0; // Ajoute le prix si la valeur est "true"
+          console.log(
+            `Ajout du prix pour la variation ${option}: ${value} - ${optionConfig.price}`,
+            finalPrice
+          );
+        } else if (typeof value === "string") {
+          if (Array.isArray(optionConfig.variations)) {
+            // Cherche la variation avec le nom correspondant
+            const variation = optionConfig.variations.find(
+              (v) => v.name === value
+            );
+            if (variation) {
+              finalPrice += variation.price;
+              validAttributes[option] = [value];
+              console.log(
+                `Ajout du prix pour la variation ${option}: ${value} - ${variation.price}`,
+                finalPrice
+              );
+            } else {
+              // Marquer comme invalide si la variation n'existe pas
+              invalidAttributes.push({ option, value });
+            }
+          } else if (optionConfig[value]) {
+            finalPrice += optionConfig[value].price;
+            validAttributes[option] = [value];
+            console.log(
+              `Ajout du prix pour ${option} avec valeur ${value}:`,
+              finalPrice
+            );
+          } else {
+            // Marquer comme invalide si l'option n'existe pas
+            invalidAttributes.push({ option, value });
+          }
+        } else if (Array.isArray(value)) {
+          // Gestion des accessoires
+          if (option === "accessories") {
+            // Filtrer les accessoires vides
+            const filteredAccessories = value.filter(
+              (accessory) => accessory !== ""
+            );
+            if (filteredAccessories.length > 0) {
+              for (const accessory of filteredAccessories) {
+                if (pricingConfigGBA.accessories[accessory]) {
+                  finalPrice += pricingConfigGBA.accessories[accessory].price;
+                  console.log(
+                    "Ajout du prix de l'accessoire:",
+                    accessory,
+                    finalPrice
+                  );
+                } else {
+                  // Marquer comme invalide si l'accessoire n'existe pas
+                  invalidAttributes.push({ accessory });
+                }
+              }
+            }
+          } else {
+            // Marquer comme invalide si l'option n'est pas reconnue
+            invalidAttributes.push({ option, value });
+          }
         }
+      } else {
+        // Marquer comme invalide si l'option n'existe pas
+        invalidAttributes.push({ option, value });
       }
     }
 
-    // Gérer les accessoires (ajouter le prix pour chaque accessoire s'il existe dans pricingConfigGBA)
-    if (customOptions.accessories && customOptions.accessories.length > 0) {
-      for (const accessory of customOptions.accessories) {
-        if (pricingConfigGBA.accessories[accessory]) {
-          finalPrice += pricingConfigGBA.accessories[accessory].price;
-          console.log("Ajout du prix de l'accessoire:", accessory, finalPrice);
-        }
-      }
+    // Si des attributs invalides sont trouvés, renvoyer une erreur HTTP 400 avec les détails
+    if (invalidAttributes.length > 0) {
+      return res.status(400).json({
+        msg: "Certains attributs sont invalides ou n'existent pas.",
+        invalidAttributes,
+      });
     }
 
-    // Envoi du produit simple à WooCommerce
-    const product = { name, customOptions, finalPrice };
-    const addToCartUrl = await sendProductToWooCommerce(product);
+    // Envoi du produit simple à WooCommerce avec les attributs valides uniquement
+    const product = { name, customOptions: validAttributes, finalPrice };
+    // Ajoute ici la logique pour envoyer le produit à WooCommerce
 
-    // Redirection vers WooCommerce pour ajouter le produit au panier
-    res.redirect(addToCartUrl);
+    res.status(200).json({ product });
   } catch (error) {
-    console.log(error);
-    res
-      .status(500)
-      .json({ msg: "Erreur lors de la création du produit. " + error.message });
+    console.error("Erreur lors de l'ajout du produit:", error);
+    res.status(500).json({ msg: "Erreur interne du serveur" });
   }
 });
 

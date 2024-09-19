2e0082325992bb6f21313079f0df3800a2ddf20d
Tim Hrdy <tim.hrdy.1@gmail.com>
2024-09-19 09:38:00 +0200

    fix

--- retrometroid/backend/server.js
+++ retrometroid/backend/server.js
@@ -1,15 +1,9 @@
 // backend/server.js
 import express, { json } from 'express';
-import { connect } from 'mongoose';
 import apiRouter from './routers/index.js';
 
 const app = express();
 app.use(json()); // Middleware to parse JSON
-
-// Connect to MongoDB
-connect("mongodb://localhost:27017/Workshop")
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
     res.send("Welcome to the API. Use /api/products for the product routes.");

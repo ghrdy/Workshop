e955646e0fc25e2d3a4f8d15fa8566646890aad3
Athe03 <gravilathenais@free.fr>
2024-11-24 23:46:40 +0100

    mise en place nest et supertest + début des tests auth

--- retrometroid/jest.config.js
+++ retrometroid/jest.config.js
@@ -1,7 +1,18 @@
 // jest.config.js
 export default {
   transform: {
-    "^.+\\.js$": "babel-jest", // Utilise babel-jest pour transpiler le code
+    "^.+\\.jsx?$": "babel-jest",
+    //"\\.[jt]sx?$": "babel-jest",
+    //"^.+\\.js$": "babel-jest", // Utilise babel-jest pour transpiler le code
   },
+  transformIgnorePatterns: ["/node_modules/(?!(foo|bar)/)", "/bar/"],
+  /*transformIgnorePatterns: [
+    '/node_modules/(?!your-module-to-transform)/',
+  ],*/
+  moduleNameMapper: {
+    "\\.css$": "identity-obj-proxy",
+    "\\.(png|jpg|jpeg|gif|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+  testEnvironment: "jsdom",
   moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"], // Ajoute les extensions que tu utilises
 };
